dd466f8847164cf9e2e86e4e63f83deb8e649c9b
jbuguy <wassimnefzi120@gmail.com>
2026-01-10 16:33:59 +0100

    fixes for the deployment

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,16 +1,17 @@
-import axios from "axios"
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api"
-})
+    baseURL:
+        import.meta.env.MODE === "development" ? "http://localhost:5001" : "/",
+});
 
 // Add auth token to all requests
 api.interceptors.request.use((config) => {
-  const user = JSON.parse(localStorage.getItem('user'));
-  if (user && user.token) {
-    config.headers.Authorization = `Bearer ${user.token}`;
-  }
-  return config;
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user && user.token) {
+        config.headers.Authorization = `Bearer ${user.token}`;
+    }
+    return config;
 });
 
 export default api;
